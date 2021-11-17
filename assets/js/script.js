e34ec33086e203ca2825db564d21ad7350bcfb62
adairconlin <adairconlin@gmail.com>
2021-11-17 13:15:41 -0500

    display meals on page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,17 @@
+let displayDishes = function(data) {
+    let targetDiv = document.querySelectorAll("#cuisines")
+    targetDiv.forEach(div => {
+        let target = div.getAttribute("target");
+        if(target === "0") {
+            div.textContent = data.results[0].title;
+        } else if(target === "1") {
+            div.textContent = data.results[1].title;
+        } else {
+            div.textContent = data.results[2].title;
+        }
+        div.removeEventListener("click", cuisineEvent);
+    })
+};
 
 let getMeals = function(cuisine) {
     let adairKey = "52217abe5a7b45b58b6466ee89a8d551";
@@ -6,16 +20,18 @@ let getMeals = function(cuisine) {
     fetch(apiUrl).then(function(response) {
         if(response.ok) {
             response.json().then(function(data) {
-                console.log(data);
+                displayDishes(data);
             })
         }
     })
 };
 
+let cuisineEvent = function(event) {
+    let choice = event.target.textContent;
+    getMeals(choice.trim());
+}
+
 let cuisines = document.querySelectorAll("#cuisines");
 cuisines.forEach(cuisine => {
-    cuisine.addEventListener("click", function(event) {
-        let choice = event.target.textContent;
-        getMeals(choice.trim());
-    })
+    cuisine.addEventListener("click", cuisineEvent)
 });
