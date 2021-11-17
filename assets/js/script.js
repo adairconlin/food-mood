1703c1f7938b447593905936bb37df18ae962c6e
adairconlin <adairconlin@gmail.com>
2021-11-17 12:25:07 -0500

    add Spoonacular API to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,21 @@
-// Adair's API key: 52217abe5a7b45b58b6466ee89a8d551
-// Bryan's API key:
-// Michael's API key:
-// Raymond's API key:
+
+let getMeals = function(cuisine) {
+    let adairKey = "52217abe5a7b45b58b6466ee89a8d551";
+    let apiUrl = "https://api.spoonacular.com/recipes/complexSearch?cuisine=" + cuisine + "&number=3&apiKey=" + adairKey;
+
+    fetch(apiUrl).then(function(response) {
+        if(response.ok) {
+            response.json().then(function(data) {
+                console.log(data);
+            })
+        }
+    })
+};
+
+let cuisines = document.querySelectorAll("#cuisines");
+cuisines.forEach(cuisine => {
+    cuisine.addEventListener("click", function(event) {
+        let choice = event.target.textContent;
+        getMeals(choice.trim());
+    })
+});
