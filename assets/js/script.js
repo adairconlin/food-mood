b45a06c1df64531f37f722e2f7bf0e399af6e358
adairconlin <adairconlin@gmail.com>
2021-11-22 21:51:34 -0500

    Mess after main merge

--- assets/js/script.js
+++ assets/js/script.js
@@ -219,13 +219,10 @@ let getRecipe = function(event) {
     let dish = $(event.target).closest("#cuisines").find(".title").text();
     // Add a variable with your own API key and replace mine in the apiUrl
     let adairKey = "52217abe5a7b45b58b6466ee89a8d551";
-<<<<<<< HEAD
     let bryanKey = "e7f051642373424f8d6926d5bbf50dcc";
     let apiUrl = "https://api.spoonacular.com/recipes/complexSearch?query=" + dish + "&fillIngredients=true&apiKey=" + adairKey;
-=======
     let adairKey2 = "11e8d764720140219f15bde44e6550be";
     let apiUrl = "https://api.spoonacular.com/recipes/complexSearch?query=" + dish + "&fillIngredients=true&apiKey=" + adairKey2;
->>>>>>> 029cc799e4f260ab0226f2ea8b84b260c2d2573b
 
     // Grab ingredient data from the specified dish
     fetch(apiUrl).then(function(response) {
